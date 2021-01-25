257b9795ad0403e9e30813c42970ac2d15172897
Cotes Chung <11371340+cotes2020@users.noreply.github.com>
2021-01-26 05:13:40 +0800

    Init commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/**/*.{html,md}',
+    './_tabs/**/*.{html,md}',
+    './*.{html,md}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
